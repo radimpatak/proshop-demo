b0313f3aafc31eb58efa97468727954aaa5dbf00
Radim Paták <radimpatak@Radims-MacBook-Pro.local>
2025-06-13 00:26:35 +0200

    JEN TEST

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
+<<<<<<< HEAD
 import React from 'react'
 
 const App = () => {
   return (
     <div>App</div>
+=======
+const App = () => {
+  return (
+    <h1>Welcome to ProShop!</h1>
+>>>>>>> f0f1757 (JEN TEST)
   )
 }
 
